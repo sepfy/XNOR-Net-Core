e7d51a7a5a94023d6b28316afbec4d8bc0afca21
sepfy <sepfy95@gmail.com>
2019-04-26 08:14:01 +0800

    pooling for test

--- layers/pooling.cc
+++ layers/pooling.cc
@@ -28,6 +28,7 @@ void Pooling::init() {
   output = new float[batch*out_w*out_h*FC];
   out_col = new float[out_w*out_h*out_channel*batch];
   im = new float[H*W*C];
+  m_delta = new float[batch*out_channel*out_w*out_h];
 }
 
 
@@ -47,6 +48,8 @@ void Pooling::forward() {
   int filter_size = FH*FW;
   int idx;
   float max;
+  float max_idx = 0;
+  max_seq.clear();
   //out_channel = FW*FH*C;
   //col_size = (out_h*out_w)*(out_channel)
   for(int i = 0; i < batch; i++) {
@@ -57,18 +60,44 @@ void Pooling::forward() {
           for(int j = 0; j < filter_size; j++) {
             idx = i*col_size + p*(out_w*out_channel) + q*out_channel + o*filter_size + j;
             //cout << out_col[idx] << " ";
-            if(out_col[idx] > max)
+            if(out_col[idx] > max) {
               //out_w*out_h*out_channel*batch
               max = out_col[idx];
+              max_idx = idx;
+            }
           }
           //cout << endl;
           output[i*out_size+p*(out_w*C)+q*C+o] = max;
+          max_seq.push_back(max_idx);
         }
   }  
 }
 
 void Pooling::backward(float *delta) {
-  m_delta = delta;
+  //m_delta = delta;
+
+
+  memset(m_delta, 0, batch*out_channel*out_w*out_h); 
+  for(int i = 0; i < max_seq.size(); i++)
+    m_delta[max_seq[i]] = delta[max_seq[i]];
+/*
+  int filter_size = FH*FW;
+  for(int i = 0; i < batch; i++) 
+    for(int p = 0; p < out_h; p++)
+      for(int q = 0; q < out_w; q++)
+        for(int o = 0; o < C; o++) 
+          for(int j = 0; j < filter_size; j++) {
+            int idx = i*out_h*out_w*C + p*out_w*C + q*C + o;
+cout << max_seq[idx] << endl;
+            if(j == max_seq[idx])
+              m_delta[idx] = delta[idx];
+            else
+              m_delta[idx] = 0.0;
+          }
+     */ 
+    
+
+  //m_delta = delta;
   //memset(grad_weight, 0, out_channel*FC*sizeof(float));
   //memset(m_delta, 0, batch*N*sizeof(float));
   //grad_weight = out_channel*FC
