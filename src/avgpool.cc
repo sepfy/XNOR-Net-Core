4e7f94853f819d42792e66a63f5ce701b89378c7
sepfy <sepfy95@gmail.com>
2020-04-06 13:55:40 +0800

    average pool for cpu

--- src/avgpool.cc
+++ src/avgpool.cc
@@ -11,20 +11,6 @@ AvgPool::AvgPool(int W, int H, int C,
   this->FC = FC;
   this->stride = stride;
 
-  if(pad == true) {
-    this->pad = 0.5*((stride - 1)*W - stride + FW);
-    out_w = W;
-    out_h = H;
-  }
-  else {
-    this->pad = 0;
-    out_w = (W - FW)/stride + 1;
-    out_h = (H - FH)/stride + 1;
-  }
-
-
-
-  out_channel = FW*FH*C;
 }
 
 AvgPool::~AvgPool() {
@@ -34,23 +20,18 @@ AvgPool::~AvgPool() {
 void AvgPool::init() {
 
 #ifdef GPU
-  output = malloc_gpu(batch*out_w*out_h*FC);
+  output = malloc_gpu(batch*C);
   m_delta = malloc_gpu(batch*H*W*C);
 #else
-  col = new float[out_w*out_h*out_channel];
-  output = new float[batch*out_w*out_h*FC];
-  out_col = new float[out_w*out_h*out_channel*batch];
-  im = new float[H*W*C];
-  m_delta = new float[batch*H*W*C];
-  delta_col = new float[batch*out_channel*out_w*out_h];
-  indexes = new float[batch*out_w*out_h*FC];
+  output = new float[batch*C];
+  m_delta = new float(batch*H*W*C);
 #endif
 
 }
 
 void AvgPool::print() {
 
-  float umem = (float)(batch*out_w*out_h*FC + batch*H*W*C)/(1024*1024);
+  float umem = (float)(batch*C + batch*H*W*C)/(1024*1024);
   printf("Avg \t %.2f \t %d x %d x %d \t %d x %d x %d \n",
                   umem, H, W, C, 1, 1, FC);
 
@@ -60,44 +41,34 @@ void AvgPool::print() {
 
 void AvgPool::forward() {
 
-  int out_w = (W + 2*pad - FW)/stride + 1;
-  int out_h = (H + 2*pad - FH)/stride + 1;
-  int offset_w = 0, offset_h = 0;
-
-  for(int b = 0; b < batch; b++) {
-    for(int i = 0; i < out_h; i++) {
-      for(int j = 0; j < out_w; j++) {
-
-        for(int k = 0; k < FC; k++) {
-
-	  float max = -1.0e+6;
-	  int max_idx = -1;
-          for(int n = 0; n < FH; n++) { 
-            for(int m = 0; m < FW; m++) {
-              int im_row = i*stride + n;
-              int im_col = j*stride + m;
-	      int idx = b*H*W*C + im_row*W*C + im_col*C + k;
-              max_idx = (input[idx] > max) ? idx : max_idx;
-              max = (input[idx] > max) ? input[idx] : max;
-            }
-          }
-
-          int out_idx = b*out_h*out_w*FC + i*out_w*FC + j*FC + k;
-          output[out_idx] = max;
-          indexes[out_idx] = max_idx;
-	}
-      }
-    }
-  }
+  memset(output, 0, sizeof(float)*batch*C);
+  for(int b = 0; b < batch; b++)
+    for(int n = 0; n < H; n++)
+      for(int m = 0; m < W; m++)
+        for(int k = 0; k < C; k++) {
+          int out_idx = b*C + k;
+          int idx = b*H*W*C + n*W*C + m*C + k;
+          output[out_idx] += input[idx];
+        }
+  
+  for(int b = 0; b < batch; b++)
+    for(int k = 0; k < C; k++)
+      output[b*C+k] /= (float)(W*H);
+   
+    
 
 }
 
 void AvgPool::backward(float *delta) {
 
-  for(int i = 0; i < out_w*out_h*FC*batch; i++) {
-    int j = indexes[i];
-    m_delta[j] = delta[i];
-  }
+  for(int b = 0; b < batch; b++)
+    for(int n = 0; n < H; n++)
+      for(int m = 0; m < W; m++)
+        for(int k = 0; k < C; k++) {
+          int out_idx = b*C + k;
+          int idx = b*H*W*C + n*W*C + m*C + k;
+	  m_delta[idx] = delta[out_idx]/(float)(H*W);
+        }
 
 }
 
